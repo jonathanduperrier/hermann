71ac63decd828dcb7ddcb944d237b46b39da4662
jonathanduperrier <jonathan.duperrier@gmail.com>
2015-04-02 13:56:45 +0200

    ./

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
-var mainApp = angular.module('mainApp', ['ngRoute', 'mod_tlv']);
+var mainApp = angular.module('mainApp', ['ngRoute', 'mod_exp', 'mod_tlv', 'hermann.experiments' ]);
+
+/**
+ * Module Routes
+ * AngularJS will handle the merging
+ * Controller for each route are managed in the corresponding <module>/controllers.js
+ */
 
 mainApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/list_exp', {
-        templateUrl: 'experiment.html',
-        controller: ''
+      when('/experiments', {
+        templateUrl: 'experiments/list.tpl.html',
+        controller: 'ListExperiment'
       }).
+      when('/experiment/:eId', {
+         templateUrl: 'experiments/detail.tpl.html', 
+         controller: 'DetailExperiment'
+	  }).
+	  when('/experiment/:eId/edit', {
+	     templateUrl: 'experiments/edit.tpl.html', 
+	     controller: 'EditExperiment'
+	  }).
       when('/timeline', {
         templateUrl: 'timeline_visual.html',
         controller: 'timeLineVisualController'
       }).
-      /*when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).*/
       otherwise({
-        redirectTo: '/list_exp'
+        redirectTo: '/experiments'
       });
   }]);
--- js/experiments.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('myapp', ['ui.bootstrap', 'angularModalService']).controller('mainController', function ($scope) {
-	$scope.getListExperiments = function(){
-		
-	};
-	
-});
