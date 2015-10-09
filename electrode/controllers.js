1b32778b63ae8a7d9df136cd254fcf475f5b7e9b
jonathanduperrier <jonathan.duperrier@gmail.com>
2015-10-09 11:40:08 +0200

    ./

--- electrode/controllers.js
+++ electrode/controllers.js
@@ -10,14 +10,25 @@ var mod_exp = angular.module( 'hermann.electrode', [
     ]);
 
 mod_exp.controller('ListElectrode', [
-  '$scope', 'electrode' ,'ModalService',
-  function($scope, electrode, ModalService) {
+  '$scope', 'electrode' ,'ModalService', 'timeLine', 'Experiment',
+  function($scope, electrode, ModalService, timeLine, Experiment) {
   	$scope.electrode = electrode.get({}, function(data){
       $scope.electrode.objects.forEach( function( elec ){
         var $type = elec.type.split('/');
         elec.type = $type[3];
         var $manufacturer = elec.manufacturer.split('/');
         elec.manufacturer = $manufacturer[3];
+        //get timeline
+        var $timeline = elec.timeline.split('/');
+        var $idTimeline = parseInt($timeline[3]);
+        elec.timeline = timeLine.get({id:$idTimeline}, function(data){
+
+          var $exp = data.experiment;
+          //get experiment
+          var $experiment = $exp.split('/');
+          var $idExperiment = $experiment[2];
+          elec.experiment = Experiment.get({id:$idExperiment});
+        });
       });
     });
   }
--- electrode/model.js
+++ electrode/model.js
@@ -12,3 +12,14 @@ electrodeServices.factory('electrode', ['$resource',
       del: { method: 'DELETE', params:{ format:'json' }, headers:{ 'Content-Type':'application/json' } }
     });
   }]);
+
+var timeLineServices = angular.module('timeLineServices', ['ngResource']);
+
+timeLineServices.factory('timeLine', ['$resource',
+  function($resource){
+    return $resource( base_url + 'notebooks/timeline/:id', {id:'@eId'}, {
+      get: {method:'GET', params:{format:'json'}, isArray:false},
+      put: {method:'PUT', params:{format:'json'}, headers:{ 'Content-Type':'application/json' }},
+      post: { method: 'POST', params:{ format:'json' }, headers:{ 'Content-Type':'application/json' } }
+    });
+  }]);
