5e41bb4302ee46218019c510cea03ae88e256e88
jonathanduperrier <jonathan.duperrier@gmail.com>
2015-11-04 18:34:13 +0100

    ./

--- timeline/controllers.js
+++ timeline/controllers.js
@@ -21,7 +21,7 @@ function ($scope, $rootScope, $compile, ModalService, $http, $q, timeLine, event
     $scope.margin_bottom_timeline = 150;
     $scope.scale_coef = 60;
 
-    $scope.TLExp_keys = [];
+    $scope.TLExp_id = [];
 
     $scope.config_defaults = {
         'CAT VISUAL INVIVO INTRA': {
@@ -151,6 +151,15 @@ function ($scope, $rootScope, $compile, ModalService, $http, $q, timeLine, event
         ],
     };
 
+
+    $scope.stopSpin = function() {
+      if($rootScope.spin == 1){
+        setTimeout(function(){ angular.element(window).spin(); }, 3500);
+      }
+      $rootScope.spin = 0;
+    };
+
+
     // get the current experiment
     $scope.experiment = Experiment.get(
         {id:$routeParams.eID},
@@ -161,9 +170,9 @@ function ($scope, $rootScope, $compile, ModalService, $http, $q, timeLine, event
                 {experiment__id: $scope.experiment.id}, 
                 function(data){
                     angular.forEach( $scope.TLExp.objects, function(value, key) {
-                        //console.log("key"+key);
-                        //console.log("id"+value.id);
-                        $scope.TLExp_keys[key] = $scope.TLExp.objects[key].id;
+                        $scope.TLExp_id[key] = $scope.TLExp.objects[key].id;
+                        $scope.TLExp.objects[key].height = $scope.margin_bottom_timeline;
+
                         // get events
                         $scope.TLExp.objects[key].events = events.get(
                             {timeline__id: $scope.TLExp.objects[key].id}, 
@@ -173,17 +182,31 @@ function ($scope, $rootScope, $compile, ModalService, $http, $q, timeLine, event
                                     timeStampStartExp = $scope.experiment.start.valueOf();
                                     timeStampEvt = $scope.TLExp.objects[key].events.objects[key2].date.valueOf();
                                     $scope.TLExp.objects[key].events.objects[key2].vPlacement = ((new Date(timeStampEvt)/1e3|0) - (new Date(timeStampStartExp)/1e3|0)) / $scope.scale_coef;
-                                    //calculation of the length of the timeline, creation array with all values
-                                    $scope.TLExp.objects[key].height = $scope.TLExp.objects[key].events.objects[key2].vPlacement + $scope.margin_bottom_timeline;
+                                    // check whether event placement is higher than current value
+                                    if( $scope.TLExp.objects[key].events.objects[key2].vPlacement > $scope.TLExp.objects[key].height){
+                                        $scope.TLExp.objects[key].height = $scope.TLExp.objects[key].events.objects[key2].vPlacement + $scope.margin_bottom_timeline;
+                                    }
                                 });
                             }
                         );
+
                         // get epochs
-                        /*$scope.TLExp.objects[key].epochs = epochs.get(
+                        $scope.TLExp.objects[key].epochs = epochs.get(
                             {timeline__id: $scope.TLExp.objects[key].id}, 
                             function(data){
+                                angular.forEach( $scope.TLExp.objects[key].epochs.objects, function(value2, key2) {
+                                    //calculation of event placement on timeline
+                                    timeStampStartExp = $scope.experiment.start.valueOf();
+                                    timeStampEpoch = $scope.TLExp.objects[key].epochs.objects[key2].start.valueOf();
+                                    $scope.TLExp.objects[key].epochs.objects[key2].vPlacement = ((new Date(timeStampEpoch)/1e3|0) - (new Date(timeStampStartExp)/1e3|0)) / $scope.scale_coef;
+                                    // check whether event placement is higher than current value
+                                    if( $scope.TLExp.objects[key].epochs.objects[key2].vPlacement > $scope.TLExp.objects[key].height){
+                                        $scope.TLExp.objects[key].height = $scope.TLExp.objects[key].epochs.objects[key2].vPlacement + $scope.margin_bottom_timeline;
+                                    }
+                                });
+                            }
+                        );
 
-                        });*/
                     });
                 }
             );
@@ -192,35 +215,31 @@ function ($scope, $rootScope, $compile, ModalService, $http, $q, timeLine, event
 
     //show dialog add event
     $scope.showDlgEvent = function( timeline, event ){
-
         // ADD
         // if we are creating an event, we initialize it here
-        //$scope.TLExp.objects[key].height = $scope.TLExp.objects[key].events.objects[key2].vPlacement + $scope.margin_bottom_timeline;
-
-        dateStartExp = $scope.experiment.start.valueOf();
-        dateEvent = new Date();
         if( event == null ){
+            dateStartExp = $scope.experiment.start.valueOf();
+            dateEvent = new Date();
             event = {
                 id : null,
                 timeline : "/notebooks/timeline/" + timeline.id,
                 text : "",
                 date : dateEvent,
                 dateFormat : dateEvent.format('dd/mm/yyyy - HH:MM'),
-                type : "",
+                type : $scope.config_defaults[$scope.experiment.type][timeline.name]['event'],
                 color : "#FFFFFF",
                 vPlacement : (((new Date(dateEvent)/1e3|0) - (new Date(dateStartExp)/1e3|0)) / $scope.scale_coef),
-                TimeLineExp : '#/timeline' + $scope.experiment.id,
-                UrlExp : window.location.hash,
             };
             // template add
-        }//else{ template edit }
+        }else{
+            //EDIT
+        }
 
         ModalService.showModal({
             templateUrl: "timeline/modal_dlg_add_event.tpl.html",
             controller: "ManageEventController",
             inputs: {
                 title: "Event information",
-                config_defaults: $scope.config_defaults,
                 config_choices: $scope.config_choices,
                 experiment_type: $scope.experiment.type,
                 timeline_name: timeline.name,
@@ -240,83 +259,74 @@ function ($scope, $rootScope, $compile, ModalService, $http, $q, timeLine, event
         angular.element(window).spin();
         $rootScope.spin = 1;
 
-        TLExp_key = $scope.TLExp_keys.indexOf(timeline.id);
+        TLExp_key = $scope.TLExp_id.indexOf(timeline.id);
 
         // if event.id is null: POST
         events.post(event, function(data){
-          $scope.TLExp.objects[TLExp_key].events.objects.push(event);
-          $scope.TLExp.objects[TLExp_key].height = event.vPlacement + $scope.margin_bottom_timeline;
-          $scope.stopSpin();
+            $scope.TLExp.objects[TLExp_key].events.objects.push(event);
+            $scope.TLExp.objects[TLExp_key].height = event.vPlacement + $scope.margin_bottom_timeline;
+            $scope.stopSpin();
         });
         // if event.id is not null: PUT
     };
 
 
     //show dialog add epoch
-    $scope.showDlgAddEpoch = function(timeline_id, date){
-      date = new Date(date);
-      var timeline_name = "";
-      angular.forEach( $scope.TLExp.objects, function(value, key) {
-        if(value.resource_uri == ("/notebooks/timeline/" + timeline_id)){
-            timeline_name = value.name;
-            //break;
+    $scope.showDlgEpoch = function(timeline, epoch){
+        if( epoch == null ){
+            dateStartExp = $scope.experiment.start.valueOf();
+            dateStartEpoch = new Date();
+            epoch = {
+                id : null,
+                timeline : "/notebooks/timeline/" + timeline.id,
+                start : dateStartEpoch,
+                dateFormat : dateStartEpoch.format('dd/mm/yyyy - HH:MM'),
+                end : null,
+                type : $scope.config_defaults[$scope.experiment.type][timeline.name]['epoch'],
+                text : "",
+                color : "#FFF600",//
+                vPlacement : (((new Date(dateStartEpoch)/1e3|0) - (new Date(dateStartExp)/1e3|0)) / $scope.scale_coef),
+                depend : null,
+            }
         }
-      });
 
-      ModalService.showModal({
-        templateUrl: "timeline/modal_dlg_add_epoch.tpl.html",
-        controller: "AddEpochController",
-        inputs: {
-          title: "Epoch information",
-          config_defaults: $scope.config_defaults,
-          config_choices: $scope.config_choices,
-          experiment_type: $scope.experiment.type,
-          timeline_name: timeline_name,
-        }
-      }).then(function(modal) {
-        modal.element.modal();
-        modal.close.then(function(result) {
-          $scope.createEpoch(timeline_id, result.text, result.type);
+        ModalService.showModal({
+            templateUrl: "timeline/modal_dlg_add_epoch.tpl.html",
+            controller: "ManageEpochController",
+            inputs: {
+                title: "Epoch information",
+                config_defaults: $scope.config_defaults,
+                config_choices: $scope.config_choices,
+                experiment_type: $scope.experiment.type,
+                timeline_name: timeline.name,
+                epoch: epoch,
+            }
+        }).then(function(modal) {
+            modal.element.modal();
+            modal.close.then(function(result) {
+                $scope.manageEpoch( timeline, result.epoch );
+            });
         });
-      });
     };
+
     //create epoch: display it in the timaline and insert it in the database
-    $scope.createEpoch = function(timeline_id, text, type){
+    $scope.manageEpoch = function(timeline, epoch){
         angular.element(window).spin();
         $rootScope.spin = 1;
 
-        var dateEvent = new Date();
-        var dateFormat = dateEvent.format('dd/mm/yyyy - HH:MM');
+        TLExp_key = $scope.TLExp_id.indexOf(timeline.id);
 
-        angular.forEach($scope.TLExp.objects, function(value, key){
-            if(timeline_id == $scope.TLExp.objects[key].id){
-                experiment_id = $scope.TLExp.objects[key].experiment;
-            }
-        });
-
-        $scope.epochObjUnique = {
-            timeline : "/notebooks/timeline/" + timeline_id,
-            text : text,
-            date : dateEvent,
-            dateFormat : dateFormat,
-            type : type,
-            color : "#FFF600",
-            TimeLineExp : '#/timeline' + experiment_id,
-            UrlExp : window.location.hash,
-        };
-
-        events.post($scope.eventObjUnique, function(data){
-          $scope.stopSpin();
+        epochs.post(epoch, function(data){
+            $scope.TLExp.objects[TLExp_key].epochs.objects.push(epoch);
+            $scope.TLExp.objects[TLExp_key].height = epoch.vPlacement + $scope.margin_bottom_timeline;
+            $scope.stopSpin();
         });
     };
 
-    $scope.stopSpin = function() {
-      if($rootScope.spin == 1){
-        setTimeout(function(){ angular.element(window).spin(); }, 3500);
-      }
-      $rootScope.spin = 0;
-    };
 });
+
+
+
 
 mod_tlv.directive('timeLineDir', function(){
   return {
@@ -332,28 +342,30 @@ mod_tlv.directive('eventDir', function(){
   };
 });
 
+mod_tlv.directive('epochDir', function(){
+  return {
+    replace: false,
+    templateUrl: 'timeline/epoch.tpl.html'
+  };
+});
+
+
+
+
 
 mod_tlv.controller('ManageEventController', [
-  '$scope', '$element', 'title', 'close', 'config_defaults', 'config_choices', 'experiment_type', 'timeline_name', 'event',
-  function($scope, $element, title, close, config_defaults, config_choices, experiment_type, timeline_name, event) {
+  '$scope', '$element', 'title', 'close', 'config_choices', 'experiment_type', 'timeline_name', 'event',
+  function($scope, $element, title, close, config_choices, experiment_type, timeline_name, event) {
 
-  $scope.text = null;
+  //$scope.event = event;
+  $scope.text = event.text;
+  $scope.type = event.type;
   $scope.date = event.date;
-  $scope.type = null;
   $scope.timeline = event.timeline;
   $scope.color = event.color;
-  $scope.TimeLineExp = event.TimeLineExp;
-  $scope.UrlExp = event.UrlExp;
   $scope.vPlacement = event.vPlacement;
-
   $scope.title = title;
-  $scope.config_defaults = config_defaults;
-  $scope.config_choices = config_choices;
-  $scope.experiment_type = experiment_type;
-  $scope.timeline_name = timeline_name;
-
-  $scope.type = $scope.config_defaults[experiment_type][timeline_name]['event'];
-  $scope.list_selection = $scope.config_choices[timeline_name];
+  $scope.list_selection = config_choices[timeline_name];
 
   //  This close function doesn't need to use jQuery or bootstrap, because
   //  the button has the 'data-dismiss' attribute.
@@ -373,8 +385,6 @@ mod_tlv.controller('ManageEventController', [
       type: $scope.type,
       timeline: $scope.timeline,
       color: $scope.color,
-      TimeLineExp: $scope.TimeLineExp,
-      UrlExp: $scope.UrlExp,
       vPlacement: $scope.vPlacement,
     }, 100); // close, but give 500ms for bootstrap to animate
   };
@@ -391,8 +401,6 @@ mod_tlv.controller('ManageEventController', [
       type: $scope.type,
       timeline: $scope.timeline,
       color: $scope.color,
-      TimeLineExp: $scope.TimeLineExp,
-      UrlExp: $scope.UrlExp,
       vPlacement: $scope.vPlacement,
     }, 100); // close, but give 500ms for bootstrap to animate
   };
@@ -400,39 +408,22 @@ mod_tlv.controller('ManageEventController', [
 
 
 
-mod_tlv.controller('AddEpochController', [
-  '$scope', '$element', 'title', 'close', 'config_defaults', 'config_choices', 'experiment_type', 'timeline_name',
-  function($scope, $element, title, close, config_defaults, config_choices, experiment_type, timeline_name) {
+mod_tlv.controller('ManageEpochController', [
+  '$scope', '$element', 'title', 'close', 'config_choices', 'experiment_type', 'timeline_name', 'epoch',
+  function($scope, $element, title, close, config_choices, experiment_type, timeline_name, epoch) {
 
-  $scope.text = null;
-  $scope.date = null;
-  $scope.type = null;
+  $scope.epoch = epoch;
   $scope.title = title;
-  $scope.config_defaults = config_defaults;
-  $scope.config_choices = config_choices;
-  $scope.experiment_type = experiment_type;
-  $scope.timeline_name = timeline_name;
+  $scope.list_selection = config_choices[timeline_name];
 
-  $scope.type = $scope.config_defaults[experiment_type][timeline_name]['epoch'];
-  $scope.list_selection = $scope.config_choices[timeline_name];
-
-  //  This close function doesn't need to use jQuery or bootstrap, because
-  //  the button has the 'data-dismiss' attribute.
-
-  $scope.beforeClose = function() {
-    if($scope.type == null){
-      $scope.msgAlert = "Please choose type to create event !";
-    } else {
-      $scope.close();
-    }
-  };
+    $scope.beforeClose = function() {
+        $scope.close();
+    };
 
   $scope.close = function() {
     close({
-      text: $scope.text,
-      date: $scope.date,
-      type: $scope.type
-    }, 100); // close, but give 500ms for bootstrap to animate
+      epoch: $scope.epoch,
+   }, 100); // close, but give 500ms for bootstrap to animate
   };
 
   //  This cancel function must use the bootstrap, 'modal' function because
@@ -442,9 +433,7 @@ mod_tlv.controller('AddEpochController', [
     $element.modal('hide');
     //  Now call close, returning control to the caller.
     close({
-      text: $scope.text,
-      date: $scope.date,
-      type: $scope.type
+      epoch: $scope.epoch,
     }, 100); // close, but give 500ms for bootstrap to animate
   };
 }]);
