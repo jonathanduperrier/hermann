300c3a775f8def9a28e10bb18587b845b27b5d03
jonathanduperrier <jonathan.duperrier@gmail.com>
2015-11-25 13:29:11 +0100

    ./

--- animal/controllers.js
+++ animal/controllers.js
@@ -25,8 +25,60 @@ mod_exp.controller('ListAnimal', [
       $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
       $scope.predicate = predicate;
     };
+
+    $scope.showDlgAnimal = function(animal){
+      if( event == null ){
+        // ADD
+        
+        edition = false;
+      } else {
+        // EDIT
+        edition = true;
+      }
+    };
   }
 ]);
+
+mod_tlv.controller('ManageAnimalController', [
+    '$scope', '$element', 'title', 'close', 'edition', 'animal',
+    function($scope, $element, title, close, edition, animal) {
+      $scope.animal = animal;
+      $scope.title = title;
+
+    $scope.beforeClose = function() {
+        //console.log($scope.dateFormat);
+        event.date = new Date($scope.event.date);
+        if($scope.event.identifier == ""){
+            $scope.msgAlert = "Identifier field is required";
+        } else {
+            $scope.close();
+        }
+    };
+
+    $scope.delete = function(){
+        $scope.del_evt = true;
+        $scope.close();
+    };
+
+    $scope.close = function() {
+        close({
+            event: $scope.event,
+            del_evt: $scope.del_evt,
+        }, 100); // close, but give 500ms for bootstrap to animate
+    };
+
+    //  This cancel function must use the bootstrap, 'modal' function because
+    //  the doesn't have the 'data-dismiss' attribute.
+    $scope.cancel = function() {
+        //  Manually hide the modal.
+        $element.modal('hide');
+        //  Now call close, returning control to the caller.
+        close({
+            event: $scope.event,
+        }, 100); // close, but give 500ms for bootstrap to animate
+    };
+
+}]);
 
 mod_exp.controller('DetailAnimal', ['$scope', '$routeParams', 'animal' ,'ModalService', function($scope, $routeParams, animal, ModalService){
     $scope.animal = animal.get( {id: $routeParams.eID}, function(data){
